2e554827444a8d7713e462ecd3a277f9441118ec
GenesisAstete <genesis.astete.a@gmail.com>
2020-06-01 18:55:39 -0400

    formulario login en html y slider

--- src/main.js
+++ src/main.js
@@ -1,24 +1,22 @@
-// Este es el punto de entrada de tu aplicacion
+// Función Slider inicio
+let slideIndex = 0;
 
-var slideIndex = 0;
+const showSlides = () => {
+  const slides = document.getElementsByClassName('carrusel');
+  const dots = document.getElementsByClassName('dot');
+  for (let i = 0; i < slides.length; i += 1) {
+    slides[i].style.display = 'none';
+  }
+  slideIndex += 1;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (let i = 0; i < dots.length; i += 1) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
+  setTimeout(showSlides, 7000);
+};
+
 showSlides();
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("carrusel");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 7000);
-}
